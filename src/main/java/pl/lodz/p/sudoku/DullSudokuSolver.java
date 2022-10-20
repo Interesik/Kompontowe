94ea36998da5cd93a73d149632ec955fd6eefd6b
236558 Kazusek Karol <236558@edu.p.lodz.pl>
2022-10-20 13:41:12 +0200

    Zad 3 Interfejs poprawić testy? Wpisz się w pom.xml

---
+++ src/main/java/pl/lodz/p/sudoku/DullSudokuSolver.java
@@ -0,0 +1,8 @@
+package pl.lodz.p.sudoku;
+
+public class DullSudokuSolver implements SudokuSolver {
+    @Override
+    public boolean solve(SudokuBoard sudokuBoard) {
+        return false;
+    }
+}
---
+++ src/test/java/pl/lodz/p/sudoku/DullSudokuSolverTest.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.sudoku;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DullSudokuSolverTest {
+
+    @Test
+    void solve() {
+        assertFalse(false);
+    }
+}

6f556e8bbc9492e4cfd4b6bc691e611e8dc84980
236558 Kazusek Karol <236558@edu.p.lodz.pl>
2022-11-03 10:50:07 +0100

    Java doc

--- src/main/java/pl/lodz/p/sudoku/SudokuRow.java
+++ src/main/java/pl/lodz/p/sudoku/SudokuRow.java
@@ -5,20 +5,23 @@ import java.util.Set;
 /**
  * Implementacja klasy SudokuRow.
  */
+
 public class SudokuRow implements SudokuVerifier {
     private SudokuField[] row;
-/**
-     * Konstruktor Klasy SudokuColumn, Generuje jedną z kolumn składających się na pole sudoku.
-     *
+
+    /**
+     * Konstruktor Klasy SudokuColumn,Generuje jedną z kolumn składających się na pole sudoku.
      * @param row jeden z 9 wierszy składających się na planszę.
      */
+
     public SudokuRow(SudokuField[] row) {
         this.row = row;
     }
-/**
+    /**
      * Funkcja verify weryfikuje poprawność uzupełnionego wiersza.
-     * @return Zwraca True w przypadku poprawnego uzupełnienia wiersza,w przeciwnym wypadku zwraca False.
+     * @return Zwraca True w przypadku poprawnego uzupełnienia wiersza,jeśli nie False.
      */
+
     @Override
     public boolean verify() {
         Set<Integer> check = new HashSet<>();
--- src/main/java/pl/lodz/p/sudoku/SudokuVerifier.java
+++ src/main/java/pl/lodz/p/sudoku/SudokuVerifier.java
@@ -1,9 +1,11 @@
 package pl.lodz.p.sudoku;
- /**
+
+/**
  * @ Interfejs SudokuVerifier.
  */
+
 public interface SudokuVerifier {
-  	 /**
+    /**
      * Funkcja verify weryfikuje poprawność uzupełnionego boxu/rzędu/kolumny.
      * @return Zwraca True w przypadku poprawnego uzupełnienia,w przeciwnym wypadku zwraca False.
      */
